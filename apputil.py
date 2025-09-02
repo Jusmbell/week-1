f8aaac7a1cd93805f4129b4e2de9b5e05ac1f211
Justin Bell <jusmbell@iu.edu>
2025-09-02 12:50:18 +0000

    Fix utility functions and update exercises

--- apputil.py
+++ apputil.py
@@ -1,3 +1,9 @@
-
-
 # add code below ...
+# Excersise 1
+def palindrome(word):
+    cleaned_word = ''.join(char.lower() for char in word if char.isalnum())
+    return cleaned_word == cleaned_word[::-1]
+# Example
+print(palindrome("Racecar"))
+
+# Excersise 2
